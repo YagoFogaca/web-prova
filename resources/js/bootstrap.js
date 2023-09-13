00a9fc92072fb36faceca07d1e765c3eeed1fe8d
YagoFogaca <yagofogaca20@gmail.com>
2023-09-13 08:32:27 -0300

    feat: instalação do bootstrap, bootstrap-icons e jquery

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,10 +4,16 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-import axios from 'axios';
-window.axios = axios;
+import axios from "axios";
+// Testar essa linha se ela pode ser removida
+import "bootstrap";
+import $ from "jquery";
+import * as bootstrap from "bootstrap";
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.bootstrap = bootstrap;
+window.$ = $;
+window.axios = axios;
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
