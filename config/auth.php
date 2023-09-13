7b76fb8ecda2c5d160aff0d7ba6745dd250e8b2f
YagoFogaca <yagofogaca20@gmail.com>
2023-09-13 16:15:44 -0300

    feat: modelagem do schema de professores e configuração de autenticação do mesmo

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'teachers' => [
+            'driver' => 'session',
+            'provider' => 'teachers',
+        ]
     ],
 
     /*
@@ -65,10 +69,10 @@ return [
             'model' => App\Models\User::class,
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+        'teachers' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Teachers::class
+        ],
     ],
 
     /*
