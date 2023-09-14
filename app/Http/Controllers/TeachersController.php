43e410669456a67182203c32f7430442c566443f
YagoFogaca <yagofogaca20@gmail.com>
2023-09-14 08:36:38 -0300

    fix: importações desnecessarias

--- app/Http/Controllers/TeachersController.php
+++ app/Http/Controllers/TeachersController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Teachers;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 
 class TeachersController extends Controller
 {
