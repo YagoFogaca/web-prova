f4726fb0745b78f4936ad59f3935c65876ca89d5
YagoFogaca <yagofogaca20@gmail.com>
2023-09-14 14:56:07 -0300

    style: implementação do container de provas

--- resources/views/pages/exams/index.blade.php
+++ resources/views/pages/exams/index.blade.php
@@ -2,5 +2,14 @@
 
 @section('content')
     <section class="card-platform">
+        <article class="d-flex justify-content-between infos">
+            <h4>Suas Provas</h4>
+            <a class="btn btn-primary btn-bd-primary">Criar prova</a>
+        </article>
+
+        <section class="container-exams">
+
+        </section>
+
     </section>
 @endsection
