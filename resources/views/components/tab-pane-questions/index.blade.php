abb4695e7c5e67e418c4ffebcf73765b69eddfbc
YagoFogaca <yagofogaca20@gmail.com>
2023-09-20 18:29:41 -0300

    style: btn para editar e deletar, e controle de criação de questão

--- resources/views/components/tab-pane-questions/index.blade.php
+++ resources/views/components/tab-pane-questions/index.blade.php
@@ -1,15 +1,28 @@
 <div class="tab-pane fade show active" id="questions" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
     <article class="d-flex justify-content-between align-items-center infos infos-variation">
         <h5>Questões</h5>
-        <a class="btn btn-primary btn-bd-primary btn-sm"
-            href="{{ route('questions.create', ['exam' => $exam['id']]) }}">Criar Questão</a>
+        @if ($exam['remaining_points'] > 0 || !$exam['remaining_points'])
+            <a class="btn btn-primary btn-bd-primary btn-sm"
+                href="{{ route('questions.create', ['exam' => $exam['id']]) }}">Criar Questão</a>
+        @endif
     </article>
 
     <div>
         @foreach ($questions as $key => $question)
             <div class="card card-question">
                 <div class="card-body">
-                    <h5 class="card-title">Questão {{ $key + 1 }}</h5>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h5 class="card-title">Questão {{ $key + 1 }}</h5>
+                        <div class="btn-group">
+                            <button type="button" class="btn" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="bi bi-three-dots-vertical"></i>
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="#">Editar</a></li>
+                                <li><a class="dropdown-item" href="#">Apagar</a></li>
+                            </ul>
+                        </div>
+                    </div>
                     <p class="card-text">{{ $question['statement'] }}</p>
                 </div>
 
